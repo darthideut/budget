871670b8626fa574d434548e7610b268c86f4f49
Hayden Smith <haydenbsmith90@gmail.com>
2015-11-30 23:03:18 -0600

    Updated to use tuples and a menu system.

--- budget.py
+++ budget.py
@@ -1,38 +1,57 @@
 #!/bin/python
 
-#Insert income amounts here
-print "Welcome. Please enter your income per month here."
-income1 = int(raw_input())
-print "Please enter any extra income here."
-income2 = int(raw_input())
+budget_menu = 0
+budget_income = []
+budget_expense =[]
+while budget_menu != 6:
+	print "--------------------------"
+	print "1. Add Income"
+	print "2. Remove Income"
+	print "3. Add Expenses"
+	print "4. Remove Expenses"
+	print "5. Totals"
+	print "6. Exit"
+	budget_menu = input("Pick an item from the menu: ")
+	
+	if budget_menu == 1:
+		print "Please name this source of this income."
+		inc_name = raw_input()
+		print "Please enter the amount of this income."
+		inc_amount =  float(raw_input())
+		budget_income.append((inc_name, inc_amount))
 
-total_income = income1 + income2
+	elif budget_menu == 2:
+		for index in range(1, ((len(budget_income)) + 1)):
+			(inc_name, inc_amount) = budget_income[index - 1]
+			print str(index) + ": " + inc_name + " (" + str(inc_amount) + ")"
+		print "Which one would you like to remove?"
+		del_index = int(raw_input())
+		budget_income.pop(del_index - 1)
+				
+	elif budget_menu == 3:
+		print "Please name this source of this expense."
+		exp_name = raw_input()
+		print "Please enter the amount of this expense."
+		exp_amount =  float(raw_input())
+		budget_expense.append((exp_name, exp_amount))
 
-print total_income
+	elif budget_menu == 4:
+		for index in range(1, ((len(budget_expense)) + 1)):
+			(exp_name, exp_amount) = budget_expense[index - 1]
+			print str(index) + ": " + exp_name + " (" + str(exp_amount) + ")"
+		print "Which one would you like to remove?"
+		del_index = int(raw_input())
+		budget_expense.pop(del_index - 1)
 
-#Insert expenses here
-print "Please enter your expenses per month here."
-print "Car: "
-car = int(raw_input())
-print "Home: "
-home = int(raw_input())
-print "Gas: "
-gas = int(raw_input())
-print "Utilities: "
-utilities = int(raw_input())
-print "Groceries: "
-groceries = int(raw_input())
-print "Insurance: "
-insurance = int(raw_input())
-print "Entertainment: "
-entertainment = int(raw_input())
-print "Retirement: "
-retirement = int(raw_input())
-
-total_expenses = car + home + gas + utilities + groceries + insurance + entertainment + retirement
-
-print total_expenses
-leftover = total_income - total_expenses
-print "Your leftover money for the month is: "
-print leftover
-print "Thank you for using this program."
+	elif budget_menu == 5:
+                sum_inc = 0.0
+                sum_exp = 0.0
+                sum_leftover = 0.0
+                for (inc_name, inc_amount) in budget_income:
+                        sum_inc = inc_amount + sum_inc
+                for (exp_name, exp_amount) in budget_expense:
+                        sum_exp = exp_amount + sum_exp
+                sum_leftover = sum_inc - sum_exp
+                print "The total amount of income is: " + str(sum_inc)
+                print "The total amount of expenses is: " + str(sum_exp)
+                print "The total amount leftover is: " + str(sum_leftover)
