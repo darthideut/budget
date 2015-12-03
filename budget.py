d296adb1ac4c3780e68385f2f7733404005ff6be
Hayden Smith <haydenbsmith90@gmail.com>
2015-12-02 23:24:22 -0600

    Added option to save a report file. Loading report file is next update.

--- budget.py
+++ budget.py
@@ -3,14 +3,15 @@
 budget_menu = 0
 budget_income = []
 budget_expense =[]
-while budget_menu != 6:
+while budget_menu != 7:
 	print "--------------------------"
 	print "1. Add Income"
 	print "2. Remove Income"
 	print "3. Add Expenses"
 	print "4. Remove Expenses"
 	print "5. Totals"
-	print "6. Exit"
+	print "6. Save Report"
+	print "7. Exit"
 	budget_menu = input("Pick an item from the menu: ")
 	
 	if budget_menu == 1:
@@ -26,7 +27,8 @@ while budget_menu != 6:
 			print str(index) + ": " + inc_name + " (" + str(inc_amount) + ")"
 		print "Which one would you like to remove?"
 		del_index = int(raw_input())
-		budget_income.pop(del_index - 1)
+                if (del_index <= len(budget_income)) and (del_index >= 1):
+                        budget_income.pop(del_index - 1)
 				
 	elif budget_menu == 3:
 		print "Please name this source of this expense."
@@ -41,7 +43,8 @@ while budget_menu != 6:
 			print str(index) + ": " + exp_name + " (" + str(exp_amount) + ")"
 		print "Which one would you like to remove?"
 		del_index = int(raw_input())
-		budget_expense.pop(del_index - 1)
+		if (del_index <= len(budget_expense)) and (del_index >= 1):
+                        budget_expense.pop(del_index - 1)
 
 	elif budget_menu == 5:
                 sum_inc = 0.0
@@ -55,3 +58,25 @@ while budget_menu != 6:
                 print "The total amount of income is: " + str(sum_inc)
                 print "The total amount of expenses is: " + str(sum_exp)
                 print "The total amount leftover is: " + str(sum_leftover)
+
+        elif budget_menu == 6:
+                filename = raw_input("File name to save: ")
+                out_file = open(filename, "w")
+                sum_inc = 0.0
+                sum_exp = 0.0
+                sum_leftover = 0.0
+                out_file.write("Budget Report" + "\n" + "INCOME:" + "\n")
+                for (inc_name, inc_amount) in budget_income:
+                        sum_inc = inc_amount + sum_inc
+                        out_file.write( inc_name + ": " + str(inc_amount) + "\n")
+                out_file.write("EXPENSES:" + "\n")
+                for (exp_name, exp_amount) in budget_expense:
+                        sum_exp = exp_amount + sum_exp
+                        out_file.write( exp_name + ": " + str(exp_amount) + "\n")
+                sum_leftover = sum_inc - sum_exp
+                out_file.write("TOTALS:" + "\n")
+                out_file.write("The total amount of income is: " + str(sum_inc) + "\n")
+                out_file.write("The total amount of expenses is: " + str(sum_exp) + "\n")
+                out_file.write("The total amount leftover is: " + str(sum_leftover))
+                out_file.close()         
+                
